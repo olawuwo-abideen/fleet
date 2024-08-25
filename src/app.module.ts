b7d030a583e3d20d4144dabebdc101b795074df4
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2024-08-25 21:26:18 +0100

    update module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { VehicleModule } from './vehicle/vehicle.module';
+import { DriverModule } from './driver/driver.module';
+import { TripModule } from './trip/trip.module';
+import { RouteModule } from './route/route.module';
+import { MaintenanceModule } from './maintenance/maintenance.module';
+import { FuelModule } from './fuel/fuel.module';
+import { IncidentModule } from './incident/incident.module';
+import { LocationModule } from './location/location.module';
+import { AnalyticsandreportModule } from './analyticsandreport/analyticsandreport.module';
+import { UserModule } from './user/user.module';
+import { NotificationModule } from './notification/notification.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, VehicleModule, DriverModule, TripModule, RouteModule, MaintenanceModule, FuelModule, IncidentModule, LocationModule, AnalyticsandreportModule, UserModule, NotificationModule],
   controllers: [AppController],
   providers: [AppService],
 })
