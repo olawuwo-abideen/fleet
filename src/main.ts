0f64201f2d3f90c9e87a02be3acc5dd923a39214
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2025-02-14 03:39:18 +0100

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,88 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+// import helmet from 'helmet';
+// // import cors from 'cors';
+// import cookieParser from 'cookie-parser';
+// import compression from 'compression';
+   
+// async function bootstrap() {
+// const app = await NestFactory.create(AppModule);
+
+// app.enableCors();
+// // app.use(cors());
+// app.use(helmet());
+// app.use(compression());
+// app.use(cookieParser());
+// const config = new DocumentBuilder()
+// .setTitle('Median')
+// .setDescription('The Fleet Management API description')
+// .setVersion('0.1')
+// .addBearerAuth()
+// .build();
+
+// const document = SwaggerModule.createDocument(app, config);
+// SwaggerModule.setup('api', app, document, {
+//     swaggerOptions: {
+//       persistAuthorization: true, 
+//     },  
+//   });
+//   // app.getHttpAdapter().get('/', (_, res) => {
+//   //   res.redirect('/api');
+//   // });
+// await app.listen(3000);
+// }
+// bootstrap();  
+
+
+
+
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import helmet from 'helmet';
-import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
-   
+
 async function bootstrap() {
-const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
 
-app.use(cors());
-app.use(helmet());
-app.use(compression());
-app.use(cookieParser());
-const config = new DocumentBuilder()
-.setTitle('Median')
-.setDescription('The Fleet Management API description')
-.setVersion('0.1')
-.addBearerAuth()
-.build();
 
-const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('api', app, document, {
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+
+  app.use(
+    helmet({
+      contentSecurityPolicy: false,
+    }),
+  );
+
+  app.use(compression());
+  app.use(cookieParser());
+
+  const config = new DocumentBuilder()
+    .setTitle('Median')
+    .setDescription('The Fleet Management API description')
+    .setVersion('0.1')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  
+ 
+  SwaggerModule.setup('docs', app, document, {
     swaggerOptions: {
-      persistAuthorization: true, 
+      persistAuthorization: true,
     },
   });
-  app.getHttpAdapter().get('/', (_, res) => {
-    res.redirect('/api');
-  });
-await app.listen(3000);
+
+  await app.listen(3000);
 }
-bootstrap();  
+
+bootstrap();
+
